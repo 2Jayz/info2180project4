a2f62eef1fefa24309f60f72b707e9c773d17c27
jaceg13 <jaygooden@hotmail.com>
2015-12-18 17:40:06 +0000

    changed connection parameters

--- registration_complete.php
+++ registration_complete.php
@@ -9,7 +9,7 @@
             // $user = $_POST["user"];
             // $pass = $_POST["pass"];
 
-            $conn = mysql_connect('localhost', 'root', '', 'cheapomail');
+            $conn = mysql_connect('127.0.0.1', 'root', '', 'cheapomail');
 
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
