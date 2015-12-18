05e2a7f665ead092e304e2a60ce01d3280f4c13a
jaceg13 <jaygooden@hotmail.com>
2015-12-18 17:50:16 +0000

    changed connection parameters

--- registration_complete.php
+++ registration_complete.php
@@ -1,26 +1,19 @@
-<html>
-    <head>
-        <title>Registration Complete</title>
-    </head>
-    <body>
-        <?php
-            // $fname = $_POST["fname"];
-            // $lname = $_POST["lname"];
-            // $user = $_POST["user"];
-            // $pass = $_POST["pass"];
+<?php
+    // $fname = $_POST["fname"];
+    // $lname = $_POST["lname"];
+    // $user = $_POST["user"];
+    // $pass = $_POST["pass"];
 
-            $conn = mysql_connect('127.0.0.1', 'root', '', 'cheapomail');
+    $conn = mysql_connect('127.0.0.1', 'root', '', 'cheapomail');
 
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            } 
-            echo "Connected successfully";
-            
-            // $connection = mysql_connect('127.0.0.1', 'comp2190SA','2015Sem1','MPMgmtDB');
-            
-            // $sql = "INSERT INTO User (id, firstname, lastname, password, username) VALUES ('$id', '$fname', '$lname', '$pass', '$user')";
-            
-            echo($fname.", ".$lname.", ".$user.", ".$pass);
-        ?>
-    </body>
-</html>
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    echo "Connected successfully";
+    
+    // $connection = mysql_connect('127.0.0.1', 'comp2190SA','2015Sem1','MPMgmtDB');
+    
+    // $sql = "INSERT INTO User (id, firstname, lastname, password, username) VALUES ('$id', '$fname', '$lname', '$pass', '$user')";
+    
+    echo($fname.", ".$lname.", ".$user.", ".$pass);
+?>
