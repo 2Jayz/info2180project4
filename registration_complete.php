825821f8ff64bf95e5823e8b3956fef4d9b1be36
jaceg13 <jaygooden@hotmail.com>
2015-12-19 00:49:33 +0000

    fixed validation

--- registration_complete.php
+++ registration_complete.php
@@ -1,17 +1,18 @@
 <?php
-    // $fname = $_POST["fname"];
-    // $lname = $_POST["lname"];
-    // $user = $_POST["user"];
-    // $pass = $_POST["pass"];
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $user = $_POST["user"];
+    $pass = $_POST["pass"];
 
     $conn = mysql_connect('127.0.0.1', 'root', '', 'cheapomail');
     
-    $sql = "INSERT INTO User (firstName, lastName, password, username) VALUES ('Jane', 'Doe', 'jd1234', 'jd1234')";
+    $sql = "INSERT INTO User (first_name, last_name, username, password) VALUES ('$fname', '$lname', '$user', '$pass')";
     
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
     echo "Connected successfully";
+    $conn->query($sql);
     
     // if(($connection->query($sql)) === TRUE){
         
